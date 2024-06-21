0ce2cf2ae177f1c9491bca0cb766ce70e2802f1f
anandhuremanan <ananduor@gmail.com>
2024-06-21 09:58:50 +0530

    Added sample API Routes

--- src/main.rs
+++ src/main.rs
@@ -2,37 +2,46 @@ use async_std::net::TcpStream;
 use dotenv::dotenv;
 use std::env;
 use tiberius::{Client, Config};
+use warp::{Filter, Rejection, Reply};
+use serde::Serialize;
 
+#[derive(Debug, Serialize)]
 struct Vehicle {
     manufacturing_year: String,
     make: String,
     model: String,
 }
 
-#[async_std::main]
-async fn main() -> anyhow::Result<()> {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv().ok();
 
-    let vehicles = get_vehicles().await?;
+    let get_vehicles = warp::path("vehicles")
+        .and(warp::get())
+        .and_then(get_vehicles_handler);
 
-    for vehicle in vehicles {
-        println!(
-            "{} {} {}",
-            vehicle.manufacturing_year, vehicle.make, vehicle.model
-        );
-    }
+    let routes = get_vehicles;
+
+    warp::serve(routes)
+        .run(([127, 0, 0, 1], 3030))
+        .await;
 
     Ok(())
 }
 
-async fn get_vehicles() -> anyhow::Result<Vec<Vehicle>> {
-    let connection_string = env::var("CONNECTION_STRING").expect("CONNECTION_STRING must be set");
+async fn get_vehicles_handler() -> Result<impl Reply, Rejection> {
+    let connection_string = env::var("CONNECTION_STRING")
+        .expect("CONNECTION_STRING must be set in .env file or environment variables");
 
-    let config = Config::from_ado_string(&connection_string)?;
-    let tcp = TcpStream::connect(config.get_addr()).await?;
-    tcp.set_nodelay(true)?;
+    let config = Config::from_ado_string(&connection_string)
+        .expect("Failed to parse connection string");
+    let tcp = TcpStream::connect(config.get_addr()).await
+        .expect("Failed to connect to SQL Server");
+    tcp.set_nodelay(true)
+        .expect("Failed to set TCP_NODELAY");
 
-    let mut client = Client::connect(config, tcp).await?;
+    let mut client = Client::connect(config, tcp).await
+        .expect("Failed to connect to SQL Server");
 
     let mut vehicles = Vec::<Vehicle>::new();
 
@@ -41,9 +50,11 @@ async fn get_vehicles() -> anyhow::Result<Vec<Vehicle>> {
             "SELECT Manufacturing_Year, Make, Model FROM [GRAMPRO\\40412].vehicles",
             &[&1i32],
         )
-        .await?
+        .await
+        .expect("Failed to execute query")
         .into_first_result()
-        .await?;
+        .await
+        .expect("Failed to get first result");
 
     for row in rows {
         let manufacturing_year: &str = row.get("Manufacturing_Year").unwrap();
@@ -57,5 +68,5 @@ async fn get_vehicles() -> anyhow::Result<Vec<Vehicle>> {
         });
     }
 
-    Ok(vehicles)
+    Ok(warp::reply::json(&vehicles))
 }
